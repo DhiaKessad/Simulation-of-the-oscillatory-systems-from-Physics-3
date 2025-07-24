563947463f95f918782451d6323d968637908d36
Kessad mohamed Dhia Eddine <berserk.forever.dhia@gmail.com>
2025-07-24 15:46:02 +0100

    Add files via upload

---
+++ worksheet2/ex6serie2.py
@@ -0,0 +1,28 @@
+#mass and two springs in series
+#didn't take into account gravity and potential energy
+import numpy as np
+import control as ctrl
+import matplotlib.pyplot as plt
+
+# Parameters
+m = 1 # mass
+k = 1# spring stiffness
+#ODE q" + 2k/m * q = 0
+F = 2 * k / m
+
+# State-space representation
+A = np.array([[0, 1],[-F, 0]])
+B = np.array([[0], [0]])
+C = np.array([[1, 0]])
+sys = ctrl.ss(A, B, C, 0)
+
+t = np.arange(0,10,0.1)
+[t_anl,q_anl,] = ctrl.initial_response(sys, T=t, X0 = [0.1,0])
+
+plt.figure(figsize=(7,7))
+plt.plot(t_anl,q_anl,'k-')
+plt.title('Exercise 6: Mass, two springs System Simulation')
+plt.grid()
+plt.xlabel('Time (s)')
+plt.ylabel('Displacement (m)')
+plt.show()
